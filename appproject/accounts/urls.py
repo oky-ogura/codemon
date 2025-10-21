97e42fbafa0f2638b267b537209c7c823c6209fa
fum0202 <oky2484811@stu.o-hara.ac.jp>
2025-10-21 10:27:28 +0900

    a

--- appproject/accounts/urls.py
+++ appproject/accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
+    # Redirect the accounts root to the student login page
+    path('', RedirectView.as_view(url='student_login/', permanent=False), name='accounts_root'),
     path('teacher_login/', views.teacher_login, name='teacher_login'),
     path('teacher_signup/', views.teacher_signup, name='teacher_signup'),
     path('student_login/', views.student_login, name='student_login'),
