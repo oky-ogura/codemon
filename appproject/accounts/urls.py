24fd2ac6ba7e267aee775c779523ed4bf620a1b7
okytabuchi <oky2484903@stu.o-hara.ac.jp>
2025-10-29 14:20:13 +0900

    アカウント

--- appproject/accounts/urls.py
+++ appproject/accounts/urls.py
@@ -10,26 +10,18 @@ urlpatterns = [
     path('student_login/', views.student_login, name='student_login'),
     path('student_signup/', views.student_signup, name='student_signup'),
     path('logout/', views.user_logout, name='logout'),
-<<<<<<< HEAD
-    path('account/', views.account_view, name='account_dashboard'),
-=======
->>>>>>> main
     path('block/', views.block_index, name='block_index'),
     path('block/save/', views.block_save, name='block_save'),
     path('system/', views.system_index, name='system_index'),
     path('system/save/', views.system_save, name='system_save'),
-<<<<<<< HEAD
-<<<<<<< HEAD
-    path('groups/create/', views.group_create, name='group_create'),
-    path('groups/add_member/', views.add_member_popup, name='group_add_member'),
-    path('groups/menu/', views.group_menu, name='group_menu'),
-    path('s_account/', views.s_account_view, name='s_account'),
-=======
->>>>>>> main
-=======
     path('system/choice/', views.system_choice, name='system_choice'),
     path('system/create/', views.system_create, name='system_create'),
     path('system/list/', views.system_list, name='system_list'),
     path('system/details/', views.system_details, name='system_details'),
->>>>>>> main
+    path('account/', views.account_view, name='account_dashboard'),
+    path('groups/create/', views.group_create, name='group_create'),
+    path('groups/add_member/', views.add_member_popup, name='group_add_member'),
+    path('groups/menu/', views.group_menu, name='group_menu'),
+    path('s_account/', views.s_account_view, name='s_account'),
+
 ]
