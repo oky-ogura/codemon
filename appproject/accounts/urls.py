8080fc38c71b7691985b90f6c5e0da840df13c43
ryoutafishy <oky2484897@stu.o-hara.ac.jp>
2025-11-11 09:56:18 +0900

    checkbox

--- appproject/accounts/urls.py
+++ appproject/accounts/urls.py
@@ -2,9 +2,7 @@ from django.urls import path
 from django.views.generic import RedirectView
 from . import views
 from django.contrib.auth import views as auth_views
-
-app_name = 'accounts'  # アプリケーション名前空間を追加
-
+app_name = 'accounts'  # アプリケーション名前空間を追加 
 urlpatterns = [
     # Redirect the accounts root to the student login page
     path('', RedirectView.as_view(url='student_login/', permanent=False), name='accounts_root'),
