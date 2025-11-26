3b6fbb509a960ce0015b56a80e1c3b62866f753c
h-mori2 <oky2484835@stu.o-hara.ac.jp>
2025-11-26 10:19:49 +0900

    新規登録画面

--- appproject/accounts/migrations/0007_add_avatar_column.py
+++ appproject/accounts/migrations/0007_add_avatar_column.py
@@ -1,20 +1,22 @@
 from django.db import migrations
 
 
+def add_avatar_if_missing(apps, schema_editor):
+    """avatarカラムが存在しない場合のみ追加"""
+    table = "account"
+    conn = schema_editor.connection
+    cursor = conn.cursor()
+    cursor.execute("PRAGMA table_info(%s)" % table)
+    cols = [row[1] for row in cursor.fetchall()]
+    if "avatar" not in cols:
+        cursor.execute('ALTER TABLE account ADD COLUMN "avatar" varchar(255) NULL;')
+
+
 class Migration(migrations.Migration):
     dependencies = [
         ("accounts", "0006_account_avatar"),
     ]
 
     operations = [
-        migrations.RunSQL(
-            sql="""
-            ALTER TABLE account
-            ADD COLUMN IF NOT EXISTS avatar VARCHAR(255);
-            """,
-            reverse_sql="""
-            ALTER TABLE account
-            DROP COLUMN IF EXISTS avatar;
-            """,
-        ),
+        migrations.RunPython(add_avatar_if_missing, reverse_code=migrations.RunPython.noop),
     ]
