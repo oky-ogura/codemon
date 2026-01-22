5c92d870942c46d2e9c08dea0954826d54393021
fum0202 <oky2484811@stu.o-hara.ac.jp>
2026-01-22 15:09:43 +0900

    デザイン

--- appproject/appproject/settings.py
+++ appproject/appproject/settings.py
@@ -121,27 +121,27 @@ db_host = str(os.getenv('DB_HOST', 'localhost'))
 db_port = str(os.getenv('DB_PORT', '5432'))
 
 # 一時的にSQLiteを使用（PostgreSQL接続のエンコーディング問題を回避）
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-# PostgreSQLを使用する場合は以下をコメント解除
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': db_name,
-#         'USER': db_user,
-#         'PASSWORD': db_password,
-#         'HOST': db_host,
-#         'PORT': db_port,
-#         'OPTIONS': {
-#             'client_encoding': 'UTF8',
-#         },
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+# PostgreSQLを使用する場合は以下をコメント解除
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': db_name,
+        'USER': db_user,
+        'PASSWORD': db_password,
+        'HOST': db_host,
+        'PORT': db_port,
+        'OPTIONS': {
+            'client_encoding': 'UTF8',
+        },
+    }
+}
 
 
 
